3065a3d1ae3f3bef17365308ab752a2b927c673a
Zawar Haider <zhaider@thewalmergroup.com>
2026-01-27 16:06:09 -0500

    more fixes

--- Practice_ec2_backend/server.js
+++ Practice_ec2_backend/server.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -17,21 +18,24 @@ app.get("/api/catfact", async (req, res) => {
     const response = await fetch("https://catfact.ninja/fact")
     const data = await response.json()
     res.json({ fact: data.fact })
-  } catch (err) {
+  } catch {
     res.status(500).json({ error: "cant fetch fact" })
   }
 })
 
 // frontend static serving
-const frontendPath = path.join(__dirname, "../frontend/dist")
+const frontendPath = path.join(
+  __dirname,
+  "../Practice_ec2_frontend/dist"
+)
 
 app.use(express.static(frontendPath))
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"))
 })
 
-=
+
 app.listen(3000, () => {
   console.log("Server listening on http://localhost:3000")
 })
